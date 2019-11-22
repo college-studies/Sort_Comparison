bb75483b9271758889329864d30354684faf0177
hsantos97 <58005145+hsantos97@users.noreply.github.com>
2019-11-22 10:33:00 -0300

    Update mainlista.c

--- Harison_Willian/mainlista.c
+++ Harison_Willian/mainlista.c
@@ -2,30 +2,51 @@
 #include<stdlib.h>
 #include<string.h>
 
+#include <time.h>
+#include <sys/times.h> //Times Funcao
+#include <sys/types.h> //Bib que define o registro TMS
+#include <unistd.h> //Biblioteca necessaria para descobrir os ciclos de clock
+
 
 //inclusão de biblioteca proprias
 #include"listaestatica.c"
-#include"carregaDados.c"
 #include"ordenacao.c"
-#include"numeros.c"
+
 	
 int main(int argc, char *argv[])
 {
+	clock_t tt;
+	//Variaveis para contagem do tempo
+	struct tms time; //Estrutura que irah guardar os dados a respeito do tempo de execucao do programa
+  	//clock_t clock;
+    	int tics_per_second; //Variavel que irah guardar os ciclos de clock por segundo do sistema
+    	//Pergunta ao SO sobre os ciclos de clock
+    	tics_per_second = sysconf(_SC_CLK_TCK);
+
+
 	tipoLista lista;
-	int numeros[1000], numeros2[10000], numeros3[100000];
+	FILE *arquivo;
+	int num;
 	
-	/*if( argc !=2 )
+	if( argc !=2 )
 	{
 		printf("FALHA NA QUANTIDADE DE ARGUMENTOS\n");
 		return 0;
-	}*/
-	
-	// FUNÇÃO PARA GERAR NUMEROS ALEATÓTIOS 
-	
-	
-	
-	inicializaLista(&lista);//inicializando a Lista
-	
+	}
+	else
+	{
+		inicializaLista(&lista, 1000);
+		
+		arquivo = fopen ("1000-arq1.txt", "r");
+		if( arquivo == NULL)
+		{
+			printf("IMPOSSIVEL ABRIR O ARQUIVO");
+			return 0;
+		}
+		while(EOF != fscanf(arquivo, "%d", &num) )
+			insereFim(&lista, num);
+	}
+			
 	
 	printf("\nimprimindo a lista\n");
 	
@@ -33,7 +54,16 @@ int main(int argc, char *argv[])
 	
 	printf("\n");
 
-	bubbleSort(&lista);
+	//bubbleSort(&lista);
+	
+	shellSort(&lista, 1000);
+
+	//Executa a funcao times para registro dos dados na estrutura time (estrutura tms)
+    	tt = times(&time);
+	printf("Clock = %f\n", (float)tt);	
+    	printf("\nTempo de uso do processador pelo processo: %f segundos\n", ( (double)time.tms_utime) / tics_per_second );
+	
+	//mergeSort(&lista, 1000);
 	
 	printf("\nIMPRIMINDO LISTA ORDENADA\n");
 	
